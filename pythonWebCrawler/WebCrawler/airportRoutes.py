b7889f1e4ee4f7095e65dcb9b8941ea297e781b3
dimitra4 <dimkos1996@yahoo.gr>
2023-05-23 18:53:50 +0300

    Update airportRoutes.py

--- pythonWebCrawler/WebCrawler/airportRoutes.py
+++ pythonWebCrawler/WebCrawler/airportRoutes.py
@@ -1,57 +1,84 @@
 # Install the Python library from https://pypi.org/project/amadeus
 from amadeus import Client, ResponseError
+from datetime import datetime
 import csv
 import pymongo
+
+
+list = [{'type': 'flight-offer', 'id': '1', 'source': 'GDS', 'instantTicketingRequired': False, 'nonHomogeneous': False, 'oneWay': False, 'lastTicketingDate': '2023-05-22', 'lastTicketingDateTime': '2023-05-22', 'numberOfBookableSeats': 4, 'itineraries': [{'duration': 'PT3H30M', 'segments': [{'departure': {'iataCode': 'ATH', 'at': '2023-05-28T10:40:00'}, 'arrival': {'iataCode': 'CDG', 'terminal': '0', 'at': '2023-05-28T13:10:00'}, 'carrierCode': 'GQ', 'number': '900', 'aircraft': {'code': '32N'}, 'operating': {'carrierCode': 'GQ'}, 'duration': 'PT3H30M', 'id': '147', 'numberOfStops': 0, 'blacklistedInEU': False}]}], 'price': {'currency': 'EUR', 'total': '71.19', 'base': '33.00', 'fees': [{'amount': '0.00', 'type': 'SUPPLIER'}, {'amount': '0.00', 'type': 'TICKETING'}], 'grandTotal': '71.19'}, 'pricingOptions': {'fareType': ['PUBLISHED'], 'includedCheckedBagsOnly': False}, 'validatingAirlineCodes': ['GQ'], 'travelerPricings': [{'travelerId': '1', 'fareOption': 'STANDARD', 'travelerType': 'ADULT', 'price': {'currency': 'EUR', 'total': '71.19', 'base': '33.00'}, 'fareDetailsBySegment': [{'segmentId': '147', 'cabin': 'ECONOMY', 'fareBasis': 'VILJOW', 'brandedFare': 'JOY', 'class': 'V', 'includedCheckedBags': {'quantity': 0}}]}]}, {'type': 'flight-offer', 'id': '2', 'source': 'GDS', 'instantTicketingRequired': False, 'nonHomogeneous': False, 'oneWay': False, 'lastTicketingDate': '2023-05-23', 'lastTicketingDateTime': '2023-05-23', 'numberOfBookableSeats': 9, 'itineraries': [{'duration': 'PT24H20M', 'segments': [{'departure': {'iataCode': 'ATH', 'at': '2023-05-28T11:45:00'}, 'arrival': {'iataCode': 'OSL', 'at': '2023-05-28T14:40:00'}, 'carrierCode': 'DY', 'number': '1885', 'aircraft': {'code': '73H'}, 'operating': {'carrierCode': 'DY'}, 'duration': 'PT3H55M', 'id': '113', 'numberOfStops': 0, 'blacklistedInEU': False}, {'departure': {'iataCode': 'OSL', 'at': '2023-05-29T08:40:00'}, 'arrival': {'iataCode': 'CDG', 'terminal': '1', 'at': '2023-05-29T11:05:00'}, 'carrierCode': 'DY', 'number': '1494', 'aircraft': {'code': '73H'}, 'operating': {'carrierCode': 'DY'}, 'duration': 'PT2H25M', 'id': '114', 'numberOfStops': 0, 'blacklistedInEU': False}]}], 'price': {'currency': 'EUR', 'total': '103.98', 'base': '71.00', 'fees': [{'amount': '0.00', 'type': 'SUPPLIER'}, {'amount': '0.00', 'type': 'TICKETING'}], 'grandTotal': '103.98', 'additionalServices': [{'amount': '45.00', 'type': 'CHECKED_BAGS'}]}, 'pricingOptions': {'fareType': ['PUBLISHED'], 'includedCheckedBagsOnly': False}, 'validatingAirlineCodes': ['DY'], 'travelerPricings': [{'travelerId': '1', 'fareOption': 'STANDARD', 'travelerType': 'ADULT', 'price': {'currency': 'EUR', 'total': '103.98', 'base': '71.00'}, 'fareDetailsBySegment': [{'segmentId': '113', 'cabin': 'ECONOMY', 'fareBasis': 'QKCALF', 'brandedFare': 'LOWFARE', 'class': 'Q', 'includedCheckedBags': {'quantity': 0}}, {'segmentId': '114', 'cabin': 'ECONOMY', 'fareBasis': 'HLF', 'brandedFare': 'LOWFARE', 'class': 'H', 'includedCheckedBags': {'quantity': 0}}]}]}, {'type': 'flight-offer', 'id': '3', 'source': 'GDS', 'instantTicketingRequired': False, 'nonHomogeneous': False, 'oneWay': False, 'lastTicketingDate': '2023-05-23', 'lastTicketingDateTime': '2023-05-23', 'numberOfBookableSeats': 9, 'itineraries': [{'duration': 'PT7H25M', 'segments': [{'departure': {'iataCode': 'ATH', 'at': '2023-05-28T12:35:00'}, 'arrival': {'iataCode': 'OSL', 'at': '2023-05-28T15:25:00'}, 'carrierCode': 'SK', 'number': '4638', 'aircraft': {'code': '73H'}, 'operating': {'carrierCode': 'SK'}, 'duration': 'PT3H50M', 'id': '133', 'numberOfStops': 0, 'blacklistedInEU': False}, {'departure': {'iataCode': 'OSL', 'at': '2023-05-28T16:50:00'}, 'arrival': {'iataCode': 'CDG', 'terminal': '1', 'at': '2023-05-28T19:00:00'}, 'carrierCode': 'SK', 'number': '839', 'aircraft': {'code': '73H'}, 'operating': {'carrierCode': 'SK'}, 'duration': 'PT2H10M', 'id': '134', 'numberOfStops': 0, 'blacklistedInEU': False}]}], 'price': {'currency': 'EUR', 'total': '108.98', 'base': '24.00', 'fees': [{'amount': '0.00', 'type': 'SUPPLIER'}, {'amount': '0.00', 'type': 'TICKETING'}], 'grandTotal': '108.98', 'additionalServices': [{'amount': '35.00', 'type': 'CHECKED_BAGS'}]}, 'pricingOptions': {'fareType': ['PUBLISHED'], 'includedCheckedBagsOnly': False}, 'validatingAirlineCodes': ['SK'], 'travelerPricings': [{'travelerId': '1', 'fareOption': 'STANDARD', 'travelerType': 'ADULT', 'price': {'currency': 'EUR', 'total': '108.98', 'base': '24.00'}, 'fareDetailsBySegment': [{'segmentId': '133', 'cabin': 'ECONOMY', 'fareBasis': 'OEOGHT', 'brandedFare': 'GOLIGHT', 'class': 'O', 'includedCheckedBags': {'quantity': 0}}, {'segmentId': '134', 'cabin': 'ECONOMY', 'fareBasis': 'OEOGHT', 'brandedFare': 'GOLIGHT', 'class': 'O', 'includedCheckedBags': {'quantity': 0}}]}]}]
 
 amadeus = Client(
     client_id='vnDUfBsJ5s7B1R8p03tjoI2xHLfVkMa9',
     client_secret='y5KuO0tRpy4CVlPp'
 )
 
-client = pymongo.MongoClient('mongodb://localhost:27017')
-
-mydb = client.WebCrawler
-
-collections = mydb.list_collection_names()
-
-print(mydb)
-print(collections)
-
-countries = mydb.Countries
-
-print(countries)
+# client = pymongo.MongoClient('mongodb://localhost:27017')
+# mydb = client.WebCrawler
+# collections = mydb.list_collection_names()
+# print(mydb)
+# print(collections)
+# countries = mydb.Countries
+# print(countries)
 
 # csv header
 fieldnames = ['name', 'iataCode']
+
+today = datetime.now()
+print("Today's date:", today)
+
+count = len(list)
 
 try:
     '''
     What are the destinations served by BLR airport?
     '''
-    response = amadeus.airport.direct_destinations.get(departureAirportCode='MAD')
-    list = response.data
-    print(response.body)
-    print(response.result)
-    print(list[0]['name'])
+    # response = amadeus.airport.direct_destinations.get(departureAirportCode='MAD')
+    # list = response.data
+    print(list)
+    #print(response.result)
+    #print(list[0]['name'])
 
     # create an empty list
     addToList = []
-
+    Dict = {}
+    print(len(list))
     for i in list:
-        res = dict((k, i[k]) for k in ['name', 'iataCode']
+        res = dict((k, i[k]) for k in ['price', 'itineraries', 'lastTicketingDate', 'lastTicketingDateTime']
                    if k in i)
-        print(res)
-        print(res['iataCode'])
-        if res.get('iataCode') != countries.find({"iataCode": res['iataCode']})[0].get('iataCode'):
-            print("yes")
+        Dict['id'] = i['id']
+        Dict['price'] = res['price']['total']
+        Dict['departure'] = 'ATH'
+        Dict['lastTicketingDate'] = res['lastTicketingDate']
+        Dict['lastTicketingDateTime'] = res['lastTicketingDateTime']
+        Dict['dateInserted'] = today.strftime("%d/%m/%Y") # dd/mm/YY
+        Dict['timeInserted'] = today.strftime("%H:%M")
+        Dict['countOfOffers'] = count
 
-        for doc1 in countries.find({"iataCode": res['iataCode']}):
-            print(doc1['iataCode'])
-            print(countries.find({"iataCode": res['iataCode']})[0].get('iataCode'))
+        print(Dict)
+        print(res['price']['total'])
+        print(res['itineraries'][0]['segments'][-1:])
+        for r in res['itineraries'][0]['segments'][-1:]:
+            res1 = dict((k, r[k]) for k in ['arrival', 'carrierCode', 'number']
+                        if k in r)
+            print(res1['arrival']['iataCode'])
+            Dict['arrival'] = res1['arrival']['iataCode']
+            Dict['carrierCode'] = res1['carrierCode']
+            Dict['numberOfFlight'] = res1['number']
 
-        addToList.append(res)
+        dict_copy = Dict.copy()
+
+        # if res.get('iataCode') != countries.find({"iataCode": res['iataCode']})[0].get('iataCode'):
+        #     print("yes")
+
+        # for doc1 in countries.find({"iataCode": res['iataCode']}):
+        #     print(doc1['iataCode'])
+        #     print(countries.find({"iataCode": res['iataCode']})[0].get('iataCode'))
+        print(Dict)
+        addToList.append(dict_copy)
+        print(addToList)
     #print(countries.find({"iataCode": "AGP"}))
     print(addToList)
+    #print(addToList[-1:])
 
     # add to mongoDB
     # x = countries.insert_many(addToList)
